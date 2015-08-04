af44104d7af14bc59ded30e60c62260e04e5d397
Stephen Gilardi <sgilardi8@hotmail.com>
2015-08-03 22:50:39 -0700

    layered code to remove matrix access locally

--- lib/binaryClockDisplay/BinaryClockDisplay.h
+++ lib/binaryClockDisplay/BinaryClockDisplay.h
@@ -12,8 +12,6 @@ class BinaryClockDisplay{
 	public:
 		BinaryClockDisplay(int LED1Blue, int LED1Green, int LED1Red, int LED2Blue, int LED2Green, int LED2Red, int LED3Blue, int LED3Green, int LED3Red);
 		void playStartUp();
-		void smile();
-		void clearColumn(int columnToRemove);
 		void displayBCDTime(int theCurrentHour, int theCurrentMinute, int theCurrentSecond);
 		void displayBinaryTime(int theCurrentHour, int theCurrentMinute, int theCurrentSecond);
 		void displayDigitalTime(int theCurrentHour, int theCurrentMinute, int theCurrentSecond);
@@ -22,10 +20,12 @@ class BinaryClockDisplay{
 	private:
 		void upDown();
 		void writeWelcome();
+		void smile();
 		void turnColumnBinary(int columnToChange, int numToConvertToBinary);
 		void showLEDBCD();
 		void showLEDBinary();
 		void showLEDDigital();
+		void clearColumn(int columnToRemove);
 		int rgbLEDs [3][3];
 };
 	
